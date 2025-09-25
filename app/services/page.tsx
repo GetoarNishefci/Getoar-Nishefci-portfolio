c641ca937483ea7d6ffe79bfe956f4e6acc0f3b7
GetoarNishefci <getoar.n1@gmail.com>
2025-09-25 14:55:03 +0200

    feat:start developing my portfolio

---
+++ app/services/page.tsx
@@ -0,0 +1,7 @@
+const Services = () =>{
+    return(
+        <div>test</div>
+    )
+}
+
+export default Services;
---
+++ components/header.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link"
+import Nav from "./nav"
+import { Button } from "./ui/button"
+
+const Header = () =>{
+    return (
+        <header className="py-8 xl:py-12">
+            <div className="container mx-auto flex justify-between items-center">
+                <Link href='/'>
+                        <h1 className="text-4xl font-semibold">
+                            Getoar<span className="text-sky-500">.</span>
+                            </h1>                
+                           </Link>
+
+                                {/*Desktop */}
+
+                <div className="hidden xl:flex items-center gap-8">
+                <Nav/> 
+                <Link href='/contact'>
+                <Button variant='default'>Hire me</Button>
+                </Link>
+                </div>
+
+                {/*Mobile */}
+                <div className="xl:hidden">
+                Mobile nav
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
